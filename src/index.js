21389210cbe22a79899f756a617cba49304e7772
이도현 <64634992+ksmfou98@users.noreply.github.com>
2021-05-29 13:34:05 +0900

    Settings : react-redux

--- src/index.js
+++ src/index.js
@@ -1,52 +1,12 @@
-// redux 에서 명심할 것!
-// reducer에서 state를 변경하는 것이 아니고 새로운 state를 반환하는 거임
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import App from "./components/App";
+import store from "./store";
 
-import { createStore } from "redux";
-
-const form = document.querySelector("form");
-const input = document.querySelector("input");
-const ul = document.querySelector("ul");
-
-const ADD_TODO = "ADD_TODO";
-const DELETE_TODO = "DELETE_TODO";
-
-const reducer = (state = [], action) => {
-  switch (action.type) {
-    case ADD_TODO:
-      return [...state, { text: action.text, id: Date.now() }];
-    case DELETE_TODO:
-      return 1;
-
-    default:
-      break;
-  }
-};
-
-const store = createStore(reducer);
-store.subscribe(() => console.log(store.getState()));
-
-const paintToDos = () => {
-  ul.innerHTML = "";
-  const toDos = store.getState();
-  toDos.forEach((toDo) => {
-    const li = document.createElement("li");
-    li.id = toDo.id;
-    li.innerText = toDo.text;
-    ul.appendChild(li);
-  });
-};
-
-store.subscribe(paintToDos);
-
-const addToDo = (text) => {
-  store.dispatch({ type: ADD_TODO, text });
-};
-
-const onSubmit = (e) => {
-  e.preventDefault();
-  const toDo = input.value;
-  input.value = "";
-  addToDo(toDo);
-};
-
-form.addEventListener("submit", onSubmit);
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
